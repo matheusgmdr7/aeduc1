11ec1b1ae8556cefadd8fcaba97e5045beceb5ca
matheusgmdr7 <matheusgmdr7@gmail.com>
2025-06-17 17:51:39 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,8 +17,8 @@ const Home: React.FC = () => {
         </div>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
           <div className="text-center">
-            <h1 className="text-5xl md:text-6xl font-bold mb-6">Associação de Reitores, Diretores e Professores, Estudantes de Nivel Superior e Funcionários de Instituições Educacionais e com qualificações e experiencias comprovadas em sua área de atuação.</h1>
-            <p className="text-xl md:text-2xl mb-8">de Instituições Educacionais</p>
+            <h1 className="text-5xl md:text-6xl font-bold mb-6">Associação de Reitores, Diretores, Professores, Estudantes de Nivel Superior e Funcionários de Instituições Educacionais</h1>
+            <p className="text-xl md:text-2xl mb-8">com qualificações e experiencias comprovadas em sua área de atuação.</p>
             <div className="flex flex-col sm:flex-row justify-center gap-4">
               <Link
                 to="/register"
